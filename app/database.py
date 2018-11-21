7c245d8eb3a1cd7a817e69ac6d8e69a624941288
wasibani-roy <roywaisibani@gmail.com>
2018-11-21 23:38:42 +0300

    Add the swagger docs

--- app/database.py
+++ app/database.py
@@ -38,15 +38,16 @@ class Database:
         for command in commands:
             self.cur.execute(command)
 
-        def create_tables(self):
+    def create_tables(self):
         """method for creating all tables"""
-        create_user_table = """CREATE TABLE IF NOT EXISTS users(
+        commands = (
+            """CREATE TABLE IF NOT EXISTS users(
                 user_id SERIAL PRIMARY KEY,
                 username VARCHAR NOT NULL,
                 email VARCHAR NOT NULL,
                 password VARCHAR NOT NULL
-            )"""
-        create_order_table = """CREATE TABLE IF NOT EXISTS orders(
+            )""",
+            """CREATE TABLE IF NOT EXISTS orders(
                 parcel_order_id SERIAL PRIMARY KEY,
                 user_id INTEGER NOT NULL,
                 destination VARCHAR NOT NULL,
@@ -56,9 +57,6 @@ class Database:
                 status VARCHAR NOT NULL,
                 deliver_status VARCHAR NULL, 
                 FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE)"""
-        commands = (
-            create_user_table,
-            create_order_table
         )
 
         for command in commands:
